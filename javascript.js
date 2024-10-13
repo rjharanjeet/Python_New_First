9eabd6d4dd9fc4a1b8415fdb4ea9d8d95475ae88
rjharanjeet <r.jharanjeet@gmail.com>
2024-10-13 21:06:13 +0530

    add javascript and learn artmatic and moudules opreateor

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//This is hello show in console
+console.log("Hello Ranjeet")
+
+// This Command is Show Arthmatic, Modules, 
+let a = 8;
+let b = 2;
+console.log("Add two number =", a+b);
+console.log("Substract two number =", a-b);
+console.log("Multiply two number =", a*b);
+console.log("Divide two number =", a/b);
+console.log("Modules two number =", b%a);
+console.log("Add two number =", a**b);
+
+//Unary Opreator
+
