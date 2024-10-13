8d7dabc7b7effd85a8e5dea589a720d15fa141a8
rjharanjeet <r.jharanjeet@gmail.com>
2024-10-13 10:19:25 +0530

    example take in string converting

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from tkinter import *
-root = Tk()
-w = Label(root, text='Nandi Iddli')
-w.pack()
-root.mainloop()
+# from tkinter import *
+# root = Tk()
+# w = Label(root, text='Nandi Iddli')
+# w.pack()
+# root.mainloop()
+
+x= 1
+y= 2.8
+z=1
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+a=float(z)
+b=int(y)
+c=complex(x)
+
+print(type(a))
+print(type(b))
+print(type(c))
