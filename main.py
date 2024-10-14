3dc9d4b3b7acce84afc046f5494823d9e6c8005b
rjharanjeet <r.jharanjeet@gmail.com>
2024-10-14 12:37:19 +0530

    Mody fy to main pyhton

--- main.py
+++ main.py
@@ -4,16 +4,16 @@
 # w.pack()
 # root.mainloop()
 
-x= 1
-y= 2.8
-z=1
+# x= 1
+# y= 2.8
+# z=1
 
 # print(type(x))
 # print(type(y))
 # print(type(z))
 
-a=float(z)
-b=int(y)
+# a=float(z)
+# b=int(y)
 c=complex(x)
 
 print(type(a))
